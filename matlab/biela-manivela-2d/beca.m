f1b7802c94f983bd1b53d2d44274afb8dbebb9f7
Artero96 <33542184+Artero96@users.noreply.github.com>
2017-11-19 20:51:41 +0100

    Automatización barras
    He conseguido hacer lo del bucle for, hay que mejorarlo por que muestra en pantalla todos los resultados para las barras y no se si es exactamente lo que queremos. La verdad es que es más sencillo de programar que en C.
    
    PD: Para tu sorpresa el desplazamiento más grande (al menos con las diferentes barras que he probado (i,k) ) se consigue cuando las barras son iguales. :)

--- matlab/biela-manivela-2d/beca.m
+++ matlab/biela-manivela-2d/beca.m
@@ -1,52 +1,72 @@
-%Lo que vamos a hacer es coger el máximo de la curva Vx2/w y le ponemos un porcentaje (que
-% también será dato de entrada) de ese máximo y vemos que rango de ángulo tenemos, para luego ir
-%a la gráfica de posición del punto x2 para ver el margen de recorrido.
-%Para poder buscar así que valores de L1 y L2 dan el mayor rango de
+%Lo que vamos a hacer es coger el mÃ¡ximo de la curva Vx2/w y le ponemos un porcentaje (que
+% tambiÃ©n serÃ¡ dato de entrada) de ese mÃ¡ximo y vemos que rango de Ã¡ngulo tenemos, para luego ir
+%a la grÃ¡fica de posiciÃ³n del punto x2 para ver el margen de recorrido.
+%Para poder buscar asÃ­ que valores de L1 y L2 dan el mayor rango de
 %movimiento
 
-%OJO TAMBIÉN HAY QUE HACER QUE LAS GRÁFICAS ESTÉN EN FUNCIÓN DEL TAMAÑO DE
-%L1 Y L2, por que de ser más grandes que para 5 y 10 no se ven.
+%OJO TAMBIÃ‰N HAY QUE HACER QUE LAS GRÃFICAS ESTÃ‰N EN FUNCIÃ“N DEL TAMAÃ‘O DE
+%L1 Y L2, por que de ser mÃ¡s grandes que para 5 y 10 no se ven.
 
-%¿Como hago para ver el valor de las variales x1,x2,...? ¿No salen en el workspace
-% por que main ahora es una función?  -> HAY QUE ESCRIBIRLAS EN MAIN
+%Â¿Como hago para ver el valor de las variales x1,x2,...? Â¿No salen en el workspace
+% por que main ahora es una funciÃ³n?  -> HAY QUE ESCRIBIRLAS EN MAIN
 % function [loquesea] y en beca en la llamada a main.
 
-%¿Aquí iría el doble bucle for para poder determinar los valores óptimos de L1 y L2?
+%Â¿AquÃ­ irÃ­a el doble bucle for para poder determinar los valores Ã³ptimos de L1 y L2?
 % De momento si escribimos el valor de L1 y L2 en el comand windows y
-% llamamos a 'beca' obtenemos como antes los resultados y las gráficas.
+% llamamos a 'beca' obtenemos como antes los resultados y las grÃ¡ficas.
 clear, clc, close all
 
-%¿Ponemos el valor de siempre para la primera iteración?
+%Â¿Ponemos el valor de siempre para la primera iteraciÃ³n?
 
-L1=5; L2=10;
+for i = 5:10
+    for k = 10:15
+        
+        L1=i; L2=k;
 
-[Vx2,theta,x2,params]=main(L1,L2);
+        [Vx2,theta,x2,params]=main(L1,L2);
 
-    [xm,im]=max(Vx2/params.omega); 
-    angulomax = theta(im);
-    porcentaje = 5; %Si queremos cambiar el porcentaje mejor cambiarlo aquí y ya está
+            [xm,im]=max(Vx2/params.omega); 
+            angulomax = theta(im);
+            porcentaje = 10; %Si queremos cambiar el porcentaje mejor cambiarlo aquÃ­ y ya estÃ¡
 
-    liminf = angulomax - porcentaje*angulomax/100;
-    limsup = angulomax + porcentaje*angulomax/100;
+            liminf = angulomax - porcentaje*angulomax/100;
+            limsup = angulomax + porcentaje*angulomax/100;
 
-    %El rango deseado es:
-    %Pasamos a grados
-    liminf = liminf*180/pi;
-    limsup = limsup*180/pi;
-    fprintf('El rango de ángulos deseado es:,\n')
-    fprintf('[%f,%f]\n',liminf,limsup)
-    liminf = round (liminf);
-    limsup = round (limsup);
-    x2min = x2(liminf);
-    x2max = x2(limsup);
-    fprintf('El rango de movimientos del punto 2 es:\n')
-    fprintf('[%f,%f]\n',x2min,x2max)
+            %El rango deseado es:
+            %Pasamos a grados
+            liminf = liminf*180/pi;
+            limsup = limsup*180/pi;
+%             fprintf('El rango de Ã¡ngulos deseado es:,\n')
+%             fprintf('[%f,%f]\n',liminf,limsup)
+            liminf = round (liminf);
+            limsup = round (limsup);
+            x2min = x2(liminf);
+            x2max = x2(limsup);
+%             fprintf('El rango de movimientos del punto 2 es:\n')
+%             fprintf('[%f,%f]\n',x2min,x2max)
+            Deltax2max = x2max - x2min;
+            Deltax2max = abs (Deltax2max);
+            Deltax2maxmax = 0;
+                
+                if (Deltax2max > Deltax2maxmax)
+                    Deltax2maxmax = Deltax2max;
+                       fprintf('El Desplazamiento max obtenido es:')
+                       fprintf('%f\n',Deltax2maxmax)
+                       fprintf('Para las longitudes:\n')
+                       fprintf('L1 = %f  L2 = %f \n',i,k)
+                    
+                else 
+                    Deltax2maxmax = Deltax2maxmax;
+                end
 
-  
-   %Todo esto dentro del doble bucle for
+    end
+    
+end
+
+
+            
    
-   %¿qué es lo que realente queremos? ¿que la zona estable sea lo más grande 
-   %y plana posible? y a partir de ahí obtener L1 y L2.
-
+   %Â¿quÃ© es lo que realente queremos? Â¿que la zona estable sea lo mÃ¡s grande 
+   %y plana posible? y a partir de ahÃ­ obtener L1 y L2.
 
 
