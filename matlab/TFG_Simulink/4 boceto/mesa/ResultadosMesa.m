c227b36121d1282364c095714995f90ae38caf18
Artero96 <33542184+Artero96@users.noreply.github.com>
2018-05-05 19:24:50 +0200

    Actualización
    He estado redactando el word con los resultados que salen ya todos correctos, y después me he puesto con el anexo de simulink. Entonces he creado un archivo igual que 'SimulaciónSimple' pero utilizando la entrada que hiciste para 'mesaparasimulink'. Estoy intentando que funcione con los angulos correctos pero no lo consigo. Además he modificado 'mesaparasimulink' para poder obtener todos los datos en matlab como 'simulaciónsimple' y los resultados de las gráficas no me convencen del todo.
    
    Un saludo.

--- matlab/TFG_Simulink/4 boceto/mesa/ResultadosMesa.m
+++ matlab/TFG_Simulink/4 boceto/mesa/ResultadosMesa.m
@@ -1,33 +1,4 @@
-%TFG_Angel_Artero
-
-%En el siguiente código se realizarán los cálculos necesarios para el
-%trabajo.
-
-clear, clc, close all
-
-%Obtener gráfica 'Configuración barras'
-% L1s = 2:0.25:5;
-% L2s = 6:0.25:8;
-% 
-% Resolver(L1s,L2s,1,0,0,0);
-
-%Una vez hemos visto las posibles configuraciones de barras posibles,
-%elegimos una configuracion en concreto y estudiamos su comportamiento en
-%posicion, velocidad, aceleracion y par. 
-% L1s = 4;
-% L2s = 7;
-% 
-% Resolver(L1s,L2s,0,0,1,1);
-
-%Realizamos una comparacion con metodos numéricos mediante dinamica, concretamente el
-%problema dinamico inverso, es necesario hacer diferenciacion con respecto
-%al anterior por que para este caso cambiamos las restricciones.
-% L1s = 4;
-% L2s = 7;
-% 
-% Resolver(L1s,L2s,0,1,0,0);
-
-sim('Copy_of_SimulacionSimple');
+sim('mesaparasimulink');
 
 
 theta=simout_ang.signals.values(:,1);
@@ -38,6 +9,7 @@ for i=1:length (theta)
     end
     theta (i) =theta (i) *(pi/180);
 end
+
 
 omega=simout_omega.signals.values(:,1);
 pos=simout_pos.signals.values(:,1);
